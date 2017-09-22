53059477628dcdceb2a01d3344644e5e9da676f3
hovanem <matt.hova@autodesk.com>
2017-09-22 12:03:30 -0700

    added dev setup for receiving faye socket updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import Faye from 'faye';
+import './main.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      client: new Faye.Client('http://localhost:3000/faye'),
+      bots: {},
+      jobs: {},
+      files: {},
+    };
+
+    this.state.client.subscribe('/bots', ({ bots }) => {
+      this.setState({ bots });
+    });
+    this.state.client.subscribe('/jobs', ({ jobs }) => {
+      this.setState({ jobs });
+    });
+    this.state.client.subscribe('/files', ({ files }) => {
+      this.setState({ files });
+    });
+  }
   render() {
     return (
       <div className="App">
-        <h2>Welcome to React</h2>
+        <h1>MCU</h1>
       </div>
     );
   }
